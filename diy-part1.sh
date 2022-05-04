872413c1e8d477bcf8ae9ce66a8f5d6d0b3b6b10
AE86JAY <AE86JAY@users.noreply.github.com>
2022-05-04 13:29:46 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,27 +23,6 @@ git clone https://github.com/xiaozhuai/luci-app-filebrowser.git ./package/diy/fi
 # App过滤
 git clone https://github.com/destan19/OpenAppFilter.git ./package/diy/OpenAppFilter
 
-# 全能推送
-git clone https://github.com/zzsj0928/luci-app-pushbot.git ./package/diy/pushbot
-
-# SSR PLUS
-# git clone https://github.com/fw876/helloworld.git ./package/diy/helloworld
-
-# AdGuardHome广告过虑
-git clone https://github.com/AdguardTeam/AdGuardHome.git ./package/diy/AdGuardHome
-
-# Luci AdGuardHome广告过虑
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/diy/luci-app-adguardhome
-
-# 简单mesh组网(210607添加）
-git clone https://github.com/ntlf9t/luci-app-easymesh.git ./package/diy/easymesh
-
-# 流量统计
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git ./package/diy/bandwidthd
-
-#  自动关机
-# git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git ./package/diy/poweroffdevice
-
 # 京东
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/diy/luci-app-jd-dailybonus
 
@@ -58,18 +37,3 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/diy/HelloWorl
 
 # IPTV
 git clone https://github.com/riverscn/openwrt-iptvhelper.git ./package/diy/iptvhelper
-
-# apfree_wifidog
-git clone https://github.com/AE86JAY/apfree_wifidog.git ./package/diy/apfree_wifidog
-
-# luci-app-apfree_wifidog
-git clone https://github.com/jmnmu/luci-app-apfree_wifidog.git ./package/diy/luci-app-apfree_wifidog
-
-# apfree_wifidog-AUTH
-git clone https://github.com/Echowxsy/wx-apfree_wifidog-auth.git ./package/diy/wx-apfree_wifidog-auth
-
-# apfree_wifidog-AUTH-LUCI
-git clone https://github.com/songzhu1028/wifidog-auth-luci.git ./package/diy/wifidog-auth-luci
-
-# 抓包
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/diy/luci-app-tcpdump
